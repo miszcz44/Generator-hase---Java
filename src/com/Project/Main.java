0025aae0d5090af03520f28c3e3b9e9ca6287b1f
Eryk Bojaruniec <erykb412@wp.pl>
2022-06-28 14:14:34 +0200

    Wypisano wszystkie prosby do uzytkownika

--- src/com/Project/Main.java
+++ src/com/Project/Main.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę znaków w swoim haśle");
+        int numberOfCharacters = scanner.nextInt();
+        System.out.println("Podaj liczbę dużych liter w haśle");
+        int numberOfUppercaseLetters = scanner.nextInt();
+        System.out.println("Podaj liczbę małych liter w haśle");
+        int numberOfLowercaseLetters = scanner.nextInt();
+        System.out.println("Podaj liczbę cyfr w haśle");
+        int numberOfDigits = scanner.nextInt();
+        System.out.println("Podaj liczbę znaków specjalnych w haśle");
+        int numberOfSpecialCharacters = scanner.nextInt();
 
     }
 }
